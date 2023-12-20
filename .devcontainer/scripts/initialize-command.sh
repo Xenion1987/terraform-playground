cc92e920c4df5726efa0f8efac39207dec29fae6
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2023-12-20 20:28:41 +0100

    init devcontainer

---
+++ .devcontainer/scripts/initialize-command.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DOCKER_VOLUME_NAME="devcontainer-history"
+
+if ! docker volume inspect "${DOCKER_VOLUME_NAME}" > /dev/null 2>&1; then
+    docker volume create "${DOCKER_VOLUME_NAME}"
+else
+    echo "INITIALIZE COMMAND :: Docker volume '${DOCKER_VOLUME_NAME}' already exists - no need to create."
+fi
---
+++ .devcontainer/scripts/post-create-command.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Install terraform autocomplete
+terraform -install-autocomplete
+# shellcheck source=~/.profile
+source ~/.profile
