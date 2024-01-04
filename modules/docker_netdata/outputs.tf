149a694ab881fce5709741b04699992002b9e6ac
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2024-01-04 15:46:01 +0000

    re-organize example files

--- modules/docker_netdata/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "volume_netdatacache_id" {
-#   value = docker_volume.netdatacache.id
-# }
-# output "volume_netdataconfig_id" {
-#   value = docker_volume.netdataconfig.id
-# }
-# output "volume_netdatalib_id" {
-#   value = docker_volume.netdatalib.id
-# }
---
+++ outputs.tf
@@ -0,0 +1,17 @@
+# This file contains all outputs
+output "volume_netdatacache_id" {
+  description = "ID for created docker volume 'netdatacache'"
+  value       = docker_volume.netdatacache.id
+}
+output "volume_netdataconfig_id" {
+  description = "ID for created docker volume 'netdataconfig'"
+  value       = docker_volume.netdataconfig.id
+}
+output "volume_netdatalib_id" {
+  description = "ID for created docker volume 'netdatalib'"
+  value       = docker_volume.netdatalib.id
+}
+output "network_id_netdata" {
+  description = "Long ID for created docker network 'netdata'"
+  value       = docker_network.netdata.id
+}
