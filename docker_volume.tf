7f411c6fc6af5c36eb3978a75f15116ce3ecb969
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2024-06-26 23:34:47 +0200

    chore: cleanup

--- docker_volume.tf
+++
@@ -1,9 +0,0 @@
-resource "docker_volume" "netdatacache" {
-  name = "netdatacache"
-}
-resource "docker_volume" "netdataconfig" {
-  name = "netdataconfig"
-}
-resource "docker_volume" "netdatalib" {
-  name = "netdatalib"
-}
